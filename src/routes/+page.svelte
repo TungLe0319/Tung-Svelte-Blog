0e9a26e58531b0c1f695dfd01d2760589ab548dc
Tung N Le <tung.le0319@gmail.com>
2023-10-05 08:06:01 -0600

    created base post component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,29 @@
     <p class="mt-4 text-md text-gray-500 font-semibold">Passionate about information-technology and development</p>
   </div>
 </div>
+
+
+<div class="posts-container  mt-2 flex flex-col justify-center bg-purple-600 p-2 ">
+<div class="text-center mb-2">
+  <h1>
+    Top 10 Recent Posts
+  </h1>
+</div>
+<div class=" post p-3 shadow-md flex space-x-2 bg-red-300 rounded-md">
+  
+    <img src="https://www.zdnet.com/a/img/resize/605df164b50d7127344cce4c41e5e2a36ac951b4/2023/04/05/e0478a88-b3ed-4516-8459-e0b919b4b2bc/artificial-intelligence.jpg?auto=webp&width=1280" alt="Blog-post " class=" post-image w-1/2 rounded-md shadow-xl shadow-slate-400">
+  
+<div class="post-body p-4 w-1/2  space-y-4  flex-col flex  items-center  bg-slate-400">
+  <div class="post-title  text-xl font-bold">
+    TITLE
+  </div>
+  <div class="post-description">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi illo, omnis quo consectetur architecto voluptatem minima atque odio ex eaque? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quos animi blanditiis quibusdam quod ipsam explicabo asperiores. Consequuntur, eveniet ex?
+  </div>
+</div>
+</div>
+
+</div>
 </div>
 
 
