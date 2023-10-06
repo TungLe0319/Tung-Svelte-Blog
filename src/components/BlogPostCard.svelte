e0c6a958158321adf6b23838c7dff1901ec1c2e5
Tung N Le <tung.le0319@gmail.com>
2023-10-06 13:13:54 -0600

    tidied up blog-post component

--- src/components/BlogPostCard.svelte
+++ src/components/BlogPostCard.svelte
@@ -4,17 +4,15 @@
 
 <!-- BlogPost.svelte -->
 
-<div class="blog-post-card p-3 my-2 shadow-md flex rounded-md">
-  <a class="flex space-x-4" href={`technology/blog/${post.id}`}>
-    <img src={post.image} alt="Blog-pos-image" class="blog-post-card-image" />
-    <div class="post-body p-4 w-1/2 flex flex-col justify-between">
-      <div class="flex-col flex items-center space-y-10">
-        <div class="post-title text-4xl font-extrabold">{post.title}</div>
-        <div class="post-subtitle text-2xl font-semibold text-gray-400">
-          {post.subtitle}
-        </div>
+<div class="blog-post-card ">
+  <a class="link-container" href={`technology/blog/${post.id}`}>
+    <img src={post.image} alt="Blog-post-img" class="blog-post-card-image" />
+    <div class="blog-post-card-body ">
+      <div class="blog-post-card-body-title-and-subtitle  ">
+        <div class="blog-post-card-title ">{post.title}</div>
+        <div class="blog-post-card-subtitle "> {post.subtitle}</div>
       </div>
-      <div class="">
+      <div class="blog-post-card-date-and-author">
         Created on {post.date} by {post.author}
       </div>
     </div>
@@ -22,8 +20,35 @@
 </div>
 
 <style lang="scss">
+
+  .link-container{
+    @apply flex space-x-4
+  }
+
+  .blog-post-card {
+    @apply  p-3 my-2 shadow-md flex rounded-md
+  }
   .blog-post-card-image {
     @apply w-1/2 rounded-md shadow-xl shadow-gray-400  transition-all   duration-150 ease-in-out
   hover:shadow-xl hover:shadow-orange-200  hover:brightness-75;
   }
+
+  .blog-post-card-body {
+    @apply  p-4 w-1/2 flex flex-col justify-between
+  } 
+
+  .blog-post-card-body-title-and-subtitle{
+    @apply flex-col  space-y-10  flex items-center 
+  }
+
+  .blog-post-card-title{
+  @apply text-4xl font-extrabold
+  }
+.blog-post-card-subtitle{
+  @apply  text-2xl font-semibold text-gray-400
+}
+
+// .blog-post-card-date-and-author{
+//   @apply 
+// }
 </style>
